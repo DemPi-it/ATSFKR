4b7a9490e218196b6c629b317fa95415b10502f4
Kirill <dempi2016@yandex.ru>
2023-06-08 13:47:14 +0300

    finishing testing system

--- src/test/java/uitests/PageObjects/HomePage.java
+++ src/test/java/uitests/PageObjects/HomePage.java
@@ -1,7 +1,5 @@
 package uitests.PageObjects;
 
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
